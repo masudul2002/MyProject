fc92f4d3698d518ca7ff8d722cf117b635677e43
MASUDUL HASAN <masudul.sstu@gmail.com>
2025-02-28 15:13:00 +0600

    04

---
+++ Comment.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+  int age;
+  // double height;
+
+  printf("Enter the age: ");
+  scanf("%d", &age);
+
+  // printf("Enter the height: ");
+  // scanf("%lf", &height);
+
+  printf("Age = %d", age);
+  // printf("\nHeight = %.1lf", height);
+
+  return 0;
+}
